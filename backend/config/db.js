62648ecb9e06f6815b6816977f04b48642ceb971
Harun mansuri <mrharun961766@gmail.com>
2026-09-06 10:30:42 +0530

    db conection done

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const ConnectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDb connect successfully');
     } catch (error) {
         console.error('MongoDb connection error', error.message);
