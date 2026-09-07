811ea3a092f8c3a0beb7ef27865b66d02a8d67d8
Harun mansuri <mrharun961766@gmail.com>
2026-09-07 10:02:44 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
-import express from express;
+import express from 'express';
 import authRoutes from './Routes/user.route.js';
 import aiRoutes from './Routes/ai.route.js';
-import ConnectDb from './config/db.js';
+import { ConnectDb } from './config/db.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 
 const app = express();
 dotenv.config();
 
 ConnectDb();
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+    }
+));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
@@ -17,6 +22,11 @@ const PORT = process.env.PORT || 5000;
 app.use('/api/auth', authRoutes);
 app.use('/api/ai', aiRoutes);
 
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+    next();
+})
 app.listen(PORT, () => {
     console.log(`Server is running on localhost:${PORT}`);
 })
