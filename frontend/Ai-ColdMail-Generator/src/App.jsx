479011d37ea42b8cccc68555a8d437b76fa6fb95
Harun mansuri <mrharun961766@gmail.com>
2026-09-09 10:02:22 +0530

    Tailwind css setup

--- frontend/Ai-ColdMail-Generator/src/App.jsx
+++ frontend/Ai-ColdMail-Generator/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-
+      <h1 className='text-blue-100 font-bold text-5xl flex-1 flex-col-reverse'>Ai Cold Mail Generator</h1>
     </>
   )
 }
